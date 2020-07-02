3f93a8498fc4f69dcefd08890b3c632e5a748b94
kevinmcguire19999 <51001719+kevinmcguire19999@users.noreply.github.com>
2020-07-02 16:21:53 -0400

    combinational function added

--- GA_folder/GA_class.py
+++ GA_folder/GA_class.py
@@ -4,7 +4,7 @@ from random import randint
 
 class percussion_class:
    
-    def __init__(self, type, seed = 1):
+    def __init__(self, type ="blank", seed = 1):
         self.name = type
         #columns_required
         self.columns = 6
@@ -19,7 +19,7 @@ class percussion_class:
         seed(self.seed)
         for i in range (8):
             cont = False
-            #checks to ensure we don't write over the same co-ordinate 
+            #checks to ensure we don't write over rahe same co-ordinate 
             while cont == False:
                 x = randint(0, self.columns -1)
                 y = randint(0, self.rows -1)
@@ -32,19 +32,31 @@ class percussion_class:
 def fitness(p_class, ideal):
     fitness_score = 0
     for y in range (0,p_class.rows):
-        print ('row:', y)
-        if sum(p_class.chromesome[y]) == sum(ideal[y]):
-            print ("same number of beats in ideal for row ", y)
-           # fitness_score += 2
         for x in range(0,p_class.columns):
-            print(p_class.chromesome[y][x],' ',  ideal [y][x])
             if p_class.chromesome[y][x] and ideal [y][x] == 1:
-                print("MATCH!")
                 fitness_score +=1*(1/(y+1))
-    
+    return (fitness_score/30)
+
+def fornicate (p1, p2):
+    new_c = [[0] * p1.columns for i in range(p1.rows)]
+    print(new_c)
+    seed (1)
+    for y in range (0,p2.rows):
+        for x in range(0,p2.columns):
+           rand = randint(0,1)
+           if rand == 0:
+               new_c[y][x] = p1.chromesome [y][x]
+           else:
+               new_c[y][x] = p2.chromesome [y][x]
+    kid = percussion_class()
+    kid.chromesome = new_c
+    print (new_c)
+    return kid
+            
+        
+
             
 
-    return (fitness_score/30)
 
 
 
@@ -52,6 +64,15 @@ def fitness(p_class, ideal):
 ideal = [[1,0,0,1,0,1],[1,0,0,1,0,0],[0,0,0,1,0,0],[1,0,1,1,0,1],[1,0,1,1,0,1]]
 ### testing class capabilities            
 bob = percussion_class("bruh", 9)
+bob2 = percussion_class("bruh2", 7)
+print(bob2.chromesome)
+print(bob.chromesome)
+kid = fornicate(bob,bob2)
+print(kid.chromesome)
+
+
+exit()
+
 print("name:")
 print(bob.name)
 print("genes:")
