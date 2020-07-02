d0085c622c00c7457fd2a73508ce1847970df63e
kevinmcguire19999 <51001719+kevinmcguire19999@users.noreply.github.com>
2020-07-02 15:25:54 -0400

    new fitness class
    extra emphasis on the first rows

--- GA_folder/GA_class.py
+++ GA_folder/GA_class.py
@@ -33,21 +33,31 @@ def fitness(p_class, ideal):
     fitness_score = 0
     for y in range (0,p_class.rows):
         print ('row:', y)
+        if sum(p_class.chromesome[y]) == sum(ideal[y]):
+            print ("same number of beats in ideal for row ", y)
+           # fitness_score += 2
         for x in range(0,p_class.columns):
             print(p_class.chromesome[y][x],' ',  ideal [y][x])
             if p_class.chromesome[y][x] and ideal [y][x] == 1:
                 print("MATCH!")
-                fitness_score +=1
+                fitness_score +=1*(1/(y+1))
+    
+            
 
-    return fitness_score
+    return (fitness_score/30)
 
 
 
 
-ideal = [[1,0,0,1,0,0],[1,0,0,1,0,0],[0,0,0,1,0,0],[1,0,1,1,0,1],[1,0,1,1,0,1]]
+ideal = [[1,0,0,1,0,1],[1,0,0,1,0,0],[0,0,0,1,0,0],[1,0,1,1,0,1],[1,0,1,1,0,1]]
 ### testing class capabilities            
 bob = percussion_class("bruh", 9)
+print("name:")
 print(bob.name)
+print("genes:")
 print(bob.chromesome)
+print("ideal:")
 print(ideal)
-print (fitness(bob,ideal))
+x= fitness(bob,ideal)
+print ("fitness score:")
+print (x)
