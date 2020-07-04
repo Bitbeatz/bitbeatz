54ba83be40cf3ef474e9eff498772a6efef86701
kevinmcguire19999 <51001719+kevinmcguire19999@users.noreply.github.com>
2020-07-03 22:39:03 -0400

    Made the Beats Fornicate
    Done

--- GA_folder/GA_class.py
+++ GA_folder/GA_class.py
@@ -1,6 +1,17 @@
 import numpy
 from random import seed
 from random import randint
+from random import shuffle
+import matplotlib.pyplot as plt
+
+seed(1)
+def Sort(sub_li): 
+  
+    # reverse = None (Sorts in Ascending order) 
+    # key is set to sort using second element of  
+    # sublist lambda has been used 
+    sub_li.sort(key = lambda x: x[1]) 
+    return sub_li 
 
 class percussion_class:
    
@@ -10,13 +21,13 @@ class percussion_class:
         self.columns = 6
         self.rows = 5
         #sets random seed for generation
-        self.seed = seed
+      
          #intializes a blank array 
         self.chromesome = [[0] * self.columns for i in range(self.rows)]
         self.gen()
 
     def gen (self):
-        seed(self.seed)
+      
         for i in range (8):
             cont = False
             #checks to ensure we don't write over rahe same co-ordinate 
@@ -39,8 +50,6 @@ def fitness(p_class, ideal):
 
 def fornicate (p1, p2, mut = 10):
     new_c = [[0] * p1.columns for i in range(p1.rows)]
-    print(new_c)
-    seed (1)
     for y in range (0,p2.rows):
         for x in range(0,p2.columns):
            rand = randint(0,1)
@@ -49,9 +58,8 @@ def fornicate (p1, p2, mut = 10):
            else:
                new_c[y][x] = p2.chromesome [y][x]
     mut_chance = randint(0,100)
-
+  
     if mut_chance >= 100-mut:
-         print("mutation occured")
          x = randint(0, p1.columns -1)
          y = randint(0, p1.rows -1)
          if new_c [y][x] != 1:
@@ -62,28 +70,75 @@ def fornicate (p1, p2, mut = 10):
     kid = percussion_class()
     kid.chromesome = new_c
     return kid
+
+def generate_population(num):
+    pop = [ percussion_class(i) for i in range(num)] 
+    return pop
+
+def get_scores(pop, goal):
+    scores = []
+    for c, value in enumerate(pop, 0):
+        scores.append([c,fitness(value, goal), value])
+    return scores
+
+
+def create_next_gen(pop, ideal):
+    #gets the top fit of the population
+    scores = get_scores(pop, ideal)
+    scores = sorted(scores, key=lambda x: x[1])
+    mates = scores[-int(len(scores)/2):]
+    next_gen = []
+    for i in range(4):
+        #shuffles parents four times to make kids
+        shuffle(mates)
+        moms = mates [-int(len(mates)/2):]
+        dads = mates [0:int(len(mates)/2)]
+        for i in range(0,len(moms)):
+            next_gen.append(fornicate(moms[i][2],dads[i][2]))
+    
+    return next_gen
+
+
+
+
+
+
+
+
             
 
 
 
 ideal = [[1,0,0,1,0,1],[1,0,0,1,0,0],[0,0,0,1,0,0],[1,0,0,0,0,0],[1,0,0,0,0,0]]
-### testing class capabilities            
-bob = percussion_class("bruh", 9)
-bob2 = percussion_class("bruh2", 7)
-print(bob2.chromesome)
-print(bob.chromesome)
-kid = fornicate(bob,bob2)
-print(kid.chromesome)
+### testing class capabilities      
+points =[]
+generation = generate_population(100)
+whole = get_scores(generation,ideal)
+scores = [item[1] for item in whole]
+score = sum(scores)/len(scores)
+print(score)
+
+for i in range(40):
+    generation = create_next_gen(generation, ideal)
+    print("Generation:", i)
+    whole = get_scores(generation,ideal)
+    scores = [item[1] for item in whole]
+    score = sum(scores)/len(scores)
+    points.append(score)
+
+plt.plot(points)
+plt.ylabel('Fitness Score')
+plt.xlabel('Generation')
+plt.show()
+    
+    
 
 
-exit()
 
-print("name:")
-print(bob.name)
-print("genes:")
-print(bob.chromesome)
-print("ideal:")
-print(ideal)
-x= fitness(bob,ideal)
-print ("fitness score:")
-print (x)
+
+
+
+
+
+
+  
